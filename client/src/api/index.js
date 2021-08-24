4c1a9a85b31de8bf490f9b10b051d39e6d7038e0
nghuyhoang2509 <nghuyhoang2509@gmail.com>
2021-08-24 16:06:07 +0700

    change uri server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const URL = process.env.NODE_ENV !== "production" ?  "https://web-tfo-demo.herokuapp.com" : "http://localhost:5000" 
+const URL = "https://web-tfo-demo.herokuapp.com"
 
 const instance = axios.create({
     baseURL: URL
