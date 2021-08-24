24f50b1d00a665b471f5d9ec7ddec3a24ec97157
nghuyhoang2509 <nghuyhoang2509@gmail.com>
2021-08-24 15:56:46 +0700

    deloy client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const URL = process.env.NODE_ENV !== "production" ? "http://localhost:5000" : "https://web-tfo-demo.herokuapp.com" 
+const URL = process.env.NODE_ENV !== "production" ?  "https://web-tfo-demo.herokuapp.com" : "http://localhost:5000" 
 
 const instance = axios.create({
     baseURL: URL
